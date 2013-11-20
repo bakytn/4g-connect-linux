cda40d234baf32fb3a44fb5a1286971b50f7d40c
Bakyt Niyazov <bakytn@gmail.com>
2013-11-20 23:11:02 +0600

    modified init.sh by adding usb_modeswitch (commented out) and the config file for this device

--- init.sh
+++ init.sh
@@ -5,3 +5,6 @@ modprobe usbserial vendor=0x12d1 product=0x14fe
 
 # use different product code based on your lsusb output
 echo "12d1 14fe" > /sys/bus/usb-serial/drivers/option1/new_id
+
+#/usr/share/usb_modeswitch
+#usb_modeswitch -v 0x12d1 -p 0x14fe -c /tmp/12d1:14fe
